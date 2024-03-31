d10ccedd81413b7f74dac031392ebfff791c7b57
cynthia <cyeeyearn@gmail.com>
2024-03-31 13:47:47 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,30 @@
+//
+const divide = (a, b) => {
+  return a / b;
+};
+
+let a = 2000;
+let b = 100;
+
+console.log(divide(a, b)); 
+
+// Defining the square arrow function
+
+const square = (num) => {
+  return num * num;
+};
+
+let x = 2;
+
+console.log(square(x)); 
+
+const add = (A, B) => {
+  return A + B;
+};
+
+// 
+let A = 3;
+let B = 4;
+
+// 
+console.log(add(A, B)); 
